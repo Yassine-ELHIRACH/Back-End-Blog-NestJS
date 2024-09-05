d8ec8013683f84a8d64a1ce9e04726cba20f5372
Yassine <yassine.elhirach@hotmail.com>
2024-09-05 17:31:53 +0200

    NestJs project files

---
+++ src/blog/schemas/blog.schema.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const BlogSchema = new mongoose.Schema({
+    title: String,
+
+    description: String,
+
+    body: String,
+
+    author: String,
+
+    date_posted: String,
+});
